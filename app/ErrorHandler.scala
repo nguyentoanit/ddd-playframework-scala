49c732a585910f04674904f99c183b7cb2f67b33
Toan Nguyen <nguyentoandev@gmail.com>
2018-11-13 17:40:46 +0700

    Error Handler

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,22 @@
+import javax.inject.Singleton
+import play.api.http.HttpErrorHandler
+import play.api.mvc.Results._
+import play.api.mvc._
+
+import scala.concurrent._
+
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,9 @@
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.18")
+logLevel := Level.Warn
+
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.20")
+
+addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.2")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
