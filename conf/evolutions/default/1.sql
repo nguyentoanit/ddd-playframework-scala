fed519defc5730b58fc0f30a739c3789bd434a18
Toan Nguyen <nguyentoandev@gmail.com>
2018-11-14 16:38:36 +0700

    Change length of article content

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,9 +2,9 @@
 CREATE TABLE articles (
     id bigint(20) NOT NULL AUTO_INCREMENT,
     title nvarchar(251) NOT NULL,
-    content text  NOT NULL,
+    content longtext  NOT NULL,
     thumbnail varchar(255) NOT NULL,
-    createOn date,
+    created_on date,
     PRIMARY KEY (id)
 );
 
